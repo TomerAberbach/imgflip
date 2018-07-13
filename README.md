190ce94c17ee3aa2e2eab9e90853d5b5fee9bacc
TomerADev <tomeraberbach@gmail.com>
2018-07-12 20:50:07 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Imgflip API Module
+A module for the interacting with the Imgflip API..
+
+[![JavaScript Style Guide](https://cdn.rawgit.com/standard/standard/master/badge.svg)](https://github.com/standard/standard)
+
+[![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+## Example
+```javascript
+const imgflip = require('./index')
+
+// https://imgflip.com/signup
+imgflip.credentials({
+  username: 'tomeraberbach',//'YOUR_USERNAME',
+  password: '988kd891sZlY'//'YOUR_PASSWORD'
+})
+
+/* https://api.imgflip.com/get_memes
+ * Get top 100 popular meme formats
+ */
+imgflip.memes().then(memes => {
+  // ID of the top meme
+  const id = memes[0]['id']
+
+  // Get URL instead using imgflip.meme, omit path argument
+  return imgflip.image('meme.jpg', id, 'MEME', 'CITY')
+})
+```
---
+++ index.js
@@ -0,0 +1,26 @@
+const request = require('request-promise-native')
+const fs = require('fs')
+
+const credentials = {
+  username: undefined,
+  password: undefined
+}
+
+const imgflip = (path, method, params) =>
+  request(`https://api.imgflip.com/${path}`, {
+    method: method,
+    qs: params,
+    json: true
+  }).then(res => res['success'] ? res['data'] : Promise.reject(new Error(res['error_message'])))
+
+module.exports.credentials = o => ['username', 'password'].forEach(key => credentials[key] = o[key])
+module.exports.memes = () => imgflip('get_memes', 'get').then(res => res['memes'])
+module.exports.meme = (id, ...captions) =>
+  imgflip('caption_image', 'post', {
+    template_id: id,
+    username: credentials.username,
+    password: credentials.password,
+    boxes: captions.map(caption => ({text: caption}))
+  }).then(res => res['url'])
+module.exports.image = (path, id, ...captions) =>
+  module.exports.meme(id, ...captions).then(url => request.get(url).pipe(fs.createWriteStream(path)))
